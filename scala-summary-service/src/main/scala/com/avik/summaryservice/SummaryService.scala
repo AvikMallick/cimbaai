98426722b313a3a22ffc054b2ec11c4d624f67ef
Avik Mallick <avikmallick111101@gmail.com>
2024-06-07 20:32:22 +0530

    added custom response for 404 from fastapi

--- scala-summary-service/src/main/scala/com/avik/summaryservice/SummaryService.scala
+++ scala-summary-service/src/main/scala/com/avik/summaryservice/SummaryService.scala
@@ -2,6 +2,7 @@ package com.avik.summaryservice
 
 import sttp.client3._
 import sttp.client3.asynchttpclient.future.AsyncHttpClientFutureBackend
+import sttp.model.StatusCode
 import io.circe.parser._
 import scala.concurrent.{Future, ExecutionContext}
 import scala.util.{Failure, Success}
@@ -13,28 +14,76 @@ object SummaryService {
   private val logger = LoggerFactory.getLogger(this.getClass)
   implicit val backend: SttpBackend[Future, Any] = AsyncHttpClientFutureBackend()
 
+//  def getSummary(url: String)(implicit ec: ExecutionContext): Future[String] = {
+//    val request = basicRequest.get(uri"${Config.Api.fastApiUrl}/summary?url=$url")
+//
+//    request.send(backend).flatMap { response =>
+//      response.body match {
+//        case Right(body) =>
+//          decode[Map[String, String]](body) match {
+//            case Right(json) => Future.successful(json("summary"))
+//            case Left(error) =>
+//              logger.error("Error decoding JSON response", error)
+//              Future.failed(new Exception(error))
+//          }
+//        case Left(error) =>
+//          logger.error("Error in HTTP response", new Exception(error))
+//          Future.failed(new Exception(error))
+//      }
+//    }.recoverWith {
+//      case ex: Exception =>
+//        logger.error("Error fetching summary", ex)
+//        Future.failed(ex)
+//    }
+//  }
+
   def getSummary(url: String)(implicit ec: ExecutionContext): Future[String] = {
+    // Create a GET request to the FastAPI service
     val request = basicRequest.get(uri"${Config.Api.fastApiUrl}/summary?url=$url")
 
+    // Send the request and handle the response
     request.send(backend).flatMap { response =>
-      response.body match {
-        case Right(body) =>
-          decode[Map[String, String]](body) match {
-            case Right(json) => Future.successful(json("summary"))
-            case Left(error) =>
-              logger.error("Error decoding JSON response", error)
-              Future.failed(new Exception(error))
-          }
-        case Left(error) =>
-          logger.error("Error in HTTP response", new Exception(error))
-          Future.failed(new Exception(error))
+
+      // Check the HTTP response status
+      if (response.code.isSuccess) {
+        // If the status is a success, handle the body of the response
+        response.body match {
+          case Right(body) =>
+            // Try to decode the body from JSON to a Map
+            decode[Map[String, String]](body) match {
+              case Right(json) =>
+                // If the decoding is successful, return the summary
+                Future.successful(json("summary"))
+              case Left(error) =>
+                // If there's an error decoding the JSON, log the error and return a failed Future
+                logger.error("Error decoding JSON response", error)
+                Future.failed(new Exception(error))
+            }
+          case Left(error) =>
+            // If there's an error in the HTTP response, log the error and return a failed Future
+            println(s"*****error in HTTP response:  $error")
+            logger.error("Error in HTTP response", new Exception(error))
+            Future.failed(new Exception(error))
+        }
+      } else {
+        // If the HTTP status is an error, check if the status code is 404
+        if (response.code == StatusCode.NotFound) {
+          // If the status code is 404, return a personalized message
+          Future.successful("The requested resource could not be found.")
+        } else {
+          // If the status code is not 404, log the status and return a failed Future
+          logger.error("HTTP error status", new Exception(response.statusText))
+          Future.failed(new Exception(response.statusText))
+        }
       }
     }.recoverWith {
+      // Handle any exceptions that occur during the Future computation
       case ex: Exception =>
         logger.error("Error fetching summary", ex)
         Future.failed(ex)
     }
   }
+
 
   def fetchAndSaveSummary(url: String)(implicit ec: ExecutionContext): CompletableFuture[String] = {
     val scalaFuture = (for {
--- summary-service/src/main/java/com/cimba/summaryservice/controller/ExceptionHandlerController.java
+++ summary-service/src/main/java/com/cimba/summaryservice/controller/ExceptionHandlerController.java
@@ -1,5 +1,6 @@
 package com.cimba.summaryservice.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +12,6 @@ public class ExceptionHandlerController {
 
     @ExceptionHandler(value = Exception.class)
     protected ResponseEntity<String> handleException() {
-        return ResponseEntity.ok("Exception occurred");
+        return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
