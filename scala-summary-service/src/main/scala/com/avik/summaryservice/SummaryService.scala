267021f8b235ce2a779551eaedd31b3ddebdcecf
Avik Mallick <avikmallick111101@gmail.com>
2024-06-07 21:53:00 +0530

    handled 404 error completely

--- scala-summary-service/src/main/scala/com/avik/summaryservice/SummaryService.scala
+++ scala-summary-service/src/main/scala/com/avik/summaryservice/SummaryService.scala
@@ -1,19 +1,16 @@
 package com.avik.summaryservice
 
-import sttp.client3._
+import io.circe.parser.*
+import sttp.client3.*
 import sttp.client3.asynchttpclient.future.AsyncHttpClientFutureBackend
 import sttp.model.StatusCode
-import io.circe.parser._
-import scala.concurrent.{Future, ExecutionContext}
-import scala.util.{Failure, Success}
-import org.slf4j.LoggerFactory
-import scala.compat.java8.FutureConverters._
+
 import java.util.concurrent.CompletableFuture
+import scala.compat.java8.FutureConverters.*
+import scala.concurrent.{ExecutionContext, Future}
 
 object SummaryService {
-  private val logger = LoggerFactory.getLogger(this.getClass)
   implicit val backend: SttpBackend[Future, Any] = AsyncHttpClientFutureBackend()
-
 
   def getSummary(url: String)(implicit ec: ExecutionContext): Future[String] = {
     // Create a GET request to the FastAPI service
@@ -34,52 +31,45 @@ object SummaryService {
                 Future.successful(json("summary"))
               case Left(error) =>
                 // If there's an error decoding the JSON, log the error and return a failed Future
-                logger.error("Error decoding JSON response", error)
                 Future.failed(new Exception(error))
             }
           case Left(error) =>
             // If there's an error in the HTTP response, log the error and return a failed Future
-            println(s"*****error in HTTP response avik1:  $error")
-            logger.error("Error in HTTP response", new Exception(error))
             Future.failed(new Exception(error))
         }
       } else {
         // If the HTTP status is an error, check if the status code is 404
         if (response.code == StatusCode.NotFound) {
           // If the status code is 404, return a personalized message
-          Future.failed(new Exception("The requested resource could not be found. it 404 error."))
+          Future.failed(new Exception("404"))
         } else {
           // If the status code is not 404, log the status and return a failed Future
-          println(s"*****error in HTTP response avik2")
-          logger.error("HTTP error status", new Exception(response.statusText))
           Future.failed(new Exception(response.statusText))
         }
       }
     }.recoverWith {
       // Handle any exceptions that occur during the Future computation
       case ex: Exception =>
-        logger.error("Error fetching summary", ex)
         Future.failed(ex)
     }
   }
 
 
-  def fetchAndSaveSummary(url: String)(implicit ec: ExecutionContext): CompletableFuture[String] = {
+  def fetchAndSaveSummary(url: String, username: String)(implicit ec: ExecutionContext)
+  : CompletableFuture[String] = {
     val scalaFuture = (for {
       _ <- Database.initSchema
       summary <- getSummary(url)
       _ <- Database.saveSummary(Summary(url, summary))
     } yield summary).recoverWith {
       case ex: Exception =>
-        logger.error("Error in fetchAndSaveSummary process", ex)
         Future.failed(ex)
     }
     scalaFuture.toJava.toCompletableFuture
   }
 
-  def shutdown(): Unit = {
-    backend.close()
-    Database.close()
-    logger.info("HTTP client backend closed.")
-  }
+//  def shutdown(): Unit = {
+//    backend.close()
+//    Database.close()
+//  }
 }
--- summary-service/src/main/java/com/cimba/summaryservice/controller/SummaryController.java
+++ summary-service/src/main/java/com/cimba/summaryservice/controller/SummaryController.java
@@ -5,7 +5,11 @@ import com.avik.summaryservice.SummaryService;
 import com.cimba.summaryservice.model.RequestDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import scala.concurrent.ExecutionContext;
 import scala.concurrent.ExecutionContextExecutor;
@@ -17,10 +21,13 @@ import java.util.concurrent.Executors;
 
 @RestController
 public class SummaryController {
-    private static final Logger logger = LoggerFactory.getLogger(SummaryController.class);
 
     @PostMapping("/get-summary")
-    public String getSummary(@RequestBody RequestDTO requestDTO) {
+    public ResponseEntity<String> getSummary(@RequestBody RequestDTO requestDTO) {
+        // Get the authenticated user's username
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        
         // Create an ExecutorService with a fixed thread pool
         ExecutorService executorService = Executors.newFixedThreadPool(10);
 
@@ -32,17 +39,23 @@ public class SummaryController {
 
         // Pass the ExecutionContext as the second argument to the fetchAndSaveSummary method
         CompletableFuture<String> futureSummary =
-                SummaryService.fetchAndSaveSummary(requestDTO.getUrl(), executionContext);
+                SummaryService.fetchAndSaveSummary(requestDTO.getUrl(), username, executionContext);
 
         try {
             // Fetch and save summary asynchronously
-            return futureSummary.exceptionally(ex -> {
-                logger.error("Error fetching summary handled", ex);
-                return "Error fetching summary. The ex ->" + ex;
-            }).get();
+            return futureSummary.thenApply(ResponseEntity::ok).exceptionally(ex -> {
+                System.out.println("****ERROR***");
+                System.out.println(ex.getMessage());
+            if(ex.getMessage().contains("404")) {
+                return new ResponseEntity<>("The given URL is not found", HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>("Error fetching summary: " + ex.getMessage(),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }).get();
         } catch (InterruptedException | ExecutionException e) {
-            logger.error("Error fetching summary", e);
-            throw new RuntimeException("Error fetching summary", e);
+            System.out.println("****ERROR 2***");
+            System.out.println(e.getMessage());
+            throw new RuntimeException("Error fetching summary. Server Error: ", e);
         }
     }
 }
