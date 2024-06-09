259dbabf9e9736454b00a967b718f5eb14704b4a
Avik Mallick <avikmallick111101@gmail.com>
2024-06-09 14:04:15 +0530

    implemented logout

--- summary-service/src/main/java/com/cimba/summaryservice/controller/AuthenticationController.java
+++ summary-service/src/main/java/com/cimba/summaryservice/controller/AuthenticationController.java
@@ -1,11 +1,15 @@
 package com.cimba.summaryservice.controller;
 
+import com.cimba.summaryservice.config.CustomLogoutHandler;
 import com.cimba.summaryservice.model.AuthenticationResponse;
 import com.cimba.summaryservice.model.User;
 import com.cimba.summaryservice.service.AuthenticationService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
     private final AuthenticationService authService;
+    private final CustomLogoutHandler customLogoutHandler;
 
-    public AuthenticationController(AuthenticationService authService) {
+    public AuthenticationController(AuthenticationService authService, CustomLogoutHandler customLogoutHandler) {
         this.authService = authService;
+        this.customLogoutHandler = customLogoutHandler;
     }
 
 
@@ -39,5 +45,12 @@ public class AuthenticationController {
             HttpServletResponse response
     ) {
         return authService.refreshToken(request, response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        customLogoutHandler.logout(request, response, auth);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
