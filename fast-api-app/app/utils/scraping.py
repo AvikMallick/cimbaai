c3fa7687b576f794383df7c4b96d5eeb43c53354
Avik Mallick <avikmallick111101@gmail.com>
2024-06-26 03:07:36 +0530

    simplified scrapping.py

--- fast-api-app/app/utils/scraping.py
+++ fast-api-app/app/utils/scraping.py
@@ -6,23 +6,7 @@ def extract_text_from_url(html_content: str) -> str:
     # Create a BeautifulSoup object
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Find all paragraph and header tags
-    tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'div', 'span', 'li', 'th', 'td',
-            'blockquote', 'pre', 'code', 'caption', 'figcaption', 'label', ]
-
-    text = []
-
-    for tag in tags:
-        # Find all elements of the current tag type in the soup object
-        elements = soup.find_all(tag)
-
-        # Iterate over each element found
-        for element in elements:
-            # Extract the text from the element and add it to the list
-            text.append(element.text)
-
-    # Join all the text elements into a single string
-    text_content = ' '.join(text)
+    text_content = soup.get_text()
 
     # Return the text content
     return text_content
