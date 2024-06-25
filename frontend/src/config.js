9351088db0a9c62fcd159ab92f3a5f4bbb6d79f0
Avik Mallick <avikmallick111101@gmail.com>
2024-06-25 22:46:06 +0530

    changed config for frontend to connect to spring locally

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:61249",
+  apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8080",
 };
 
 export default config;
